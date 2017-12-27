619d14e3f259df3d2139940ed4ede33bc4780293
johanesteves <jsesteves91@gmail.com>
2017-12-26 19:03:45 -0500

    Done.

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,19 @@
 // Code ThemedDecoration Component Here
+import React from 'react';
+
+class ThemedDecoration extends React.Component{
+  render(){
+    const theme = this.props.theme;
+    const childrenWithTheme = React.Children.map(this.props.children, function (child) {
+      return (<p className = {theme}> {child} </p>)
+    });
+
+    return(
+      <div>
+          {childrenWithTheme}
+      </div>
+    )
+  }
+}
+
+export default ThemedDecoration
